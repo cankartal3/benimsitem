ad6760695313130184abc64a5273ed8398b29f90
Uğur Can KARTAL <ugurcankartal9@gmail.com>
2022-02-02 02:12:59 +0300

    Bilgilerim adında app oluşturuldu ve databasete Hakkımda adında bir stun oluşturuldu.

---
+++ bilgilerim/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Hakkımda(models.Model):
+    STATUS = (
+        ('True' , 'Evet'),
+        ('False', 'Hayır'),
+    )
+    isim = models.CharField(max_length=30)
+    soyisim = models.CharField(max_length=30)
+    aciklama = models.CharField(max_length=300)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10,choices=STATUS)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self',blank=True, null=True,related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.isim
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('bilgilerim/', include('bilgilerim.urls')),
     path('admin/', admin.site.urls),
-    path('', include('home.urls')), # path('home', include('home.urls')),
+    path('', include('home.urls')),
+    path('home/', include('home.urls')),
 ]
